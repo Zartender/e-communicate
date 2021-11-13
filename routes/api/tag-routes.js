d7517ebb08cff028efdd1cd6fbd384e9a4ce1622
Ian Greenblott <imgreenblott@gmail.com>
2021-11-12 23:20:34 -0500

    forgot to commit with message on last push.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
+router.get('/', async (req, res) => {
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // Let's make this work.
+      include: [{ model: ProductTag, through: Product, as: 'test-products' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No available product' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', (req, res) => {
@@ -21,7 +42,7 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
