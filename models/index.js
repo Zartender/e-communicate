c193cbe8625623fe23581c0f8b4889c9e1ad9a3d
Ian Greenblott <imgreenblott@gmail.com>
2021-11-13 00:58:39 -0500

    SContinuing configuration of routes.

--- models/index.js
+++ models/index.js
@@ -3,18 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedProducts = require('../seeds/product-seeds');
+const seedCategories = require('../seeds/category-seeds');
 
 // Products belongsTo Category
+// Product.belongsToMany(Category,  {
+//   through: {
+//     model: Trip,
+//     unique: false
+//   },
+//   as: 'merch'
+// });
+// // Categories have many Products
+// seedCategories.hasMany(seedProducts)
+// // Products belongToMany Tags (through ProductTag)
 
-// Categories have many Products
+// // Tags belongToMany Products (through ProductTag)
 
-// Products belongToMany Tags (through ProductTag)
+// module.exports = {
+//   Product,
+//   Category,
+//   Tag,
+//   ProductTag,
+// };
 
-// Tags belongToMany Products (through ProductTag)
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+// module.exports = { Product, Category, Tag, ProductTag, seedProducts };
