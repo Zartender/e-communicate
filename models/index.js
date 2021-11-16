d8ac44c2a0436f5816705b2297d064d5ec466259
Ian Greenblott <imgreenblott@gmail.com>
2021-11-16 16:16:06 -0500

    Uploading core files.

--- models/index.js
+++ models/index.js
@@ -3,29 +3,27 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const seedProducts = require('../seeds/product-seeds');
-const seedCategories = require('../seeds/category-seeds');
+// const seedProducts = require('../seeds/product-seeds');
+// const seedCategories = require('../seeds/category-seeds');
 
 // Products belongsTo Category
-// Product.belongsToMany(Category,  {
-//   through: {
-//     model: Trip,
-//     unique: false
-//   },
-//   as: 'merch'
-// });
+Product.belongsToMany(Category,  {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'merch'
+});
+
 // // Categories have many Products
 // seedCategories.hasMany(seedProducts)
-// // Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag)
 
 // // Tags belongToMany Products (through ProductTag)
 
-// module.exports = {
-//   Product,
-//   Category,
-//   Tag,
-//   ProductTag,
-// };
-
-
-// module.exports = { Product, Category, Tag, ProductTag, seedProducts };
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
